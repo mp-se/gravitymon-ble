c9cfd285f8e1919d25b0396ab43fb3ed0527f1b3
Magnus <mp-se@users.noreply.github.com>
2023-12-10 15:05:33 +0100

    Updated targets for clients and servers

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,56 @@
 #include <Arduino.h>
 #include <ArduinoLog.h>
 #include <ble.hpp>
+#include <blescanner.hpp>
 
 BleSender* myBleSender;
-
-//#define BROADCAST_TILT
-//#define BROADCAST_TILT_PRO
-#define BROADCAST_GRAVITYMON
+char chip[20];
 
 void setup() {
   Serial.begin(115200);
   Log.begin(LOG_LEVEL_NOTICE, &Serial, true);
 
+  uint32_t chipId = 0;
+  for (int i = 0; i < 17; i = i + 8) {
+    chipId |= ((ESP.getEfuseMac() >> (40 - i)) & 0xff) << i;
+  }
+  snprintf(&chip[0], sizeof(chip), "%6x", chipId);
+  Log.notice(F("Main: Started setup for %s." CR), &chip[0]);
+
+
+#if defined(SERVER_TILT) || defined(SERVER_TILT_PRO) || defined(SERVER_GRAVITYMON)
+  Log.info(F("Running in BROADCAST mode!" CR));
   myBleSender = new BleSender();
+#endif
+
+#if defined(CLIENT)
+  bleScanner.init();
+#endif
+
   Log.info(F("Setup completed!" CR));
 }
 
 int loopCounter = 0;
 
 void loop() {
-#if defined(BROADCAST_TILT)
+#if defined(SERVER_TILT)
   Log.info(F("Tilt beacon started" CR));
   String color("red");
   myBleSender->sendTiltData(color, 41.234, loopCounter*1.1, false);
   delay(5000);
 #endif
 
-#if defined(BROADCAST_TILT_PRO)
+#if defined(SERVER_TILT_PRO)
   Log.info(F("Tilt PRO beacon started" CR));
   String color("red");
   myBleSender->sendTiltData(color, 41.234, 1.2345, true);
   delay(5000);
 #endif
 
-#if defined(BROADCAST_GRAVITYMON)
+#if defined(SERVER_GRAVITYMON)
   Log.info(F("Gravitymon beacon started" CR));
   String param(loopCounter);
-  String json("{\"name\":\"my_device_name\",\"ID\": \"112233\",\"token\":\"my_token\",\"interval\":" + String(loopCounter) + ",\"temperature\":20.2,\"temp_units\":\"C\",\"gravity\":1.05,\"angle\":34.45,\"battery\":3.85,\"RSSI\":-76.2}");
+  String json("{\"name\":\"my_device_name\",\"ID\": \"" + String(chip) + "\",\"token\":\"my_token\",\"interval\":" + String(loopCounter) + ",\"temperature\":20.2,\"temp_units\":\"C\",\"gravity\":1.05,\"angle\":34.45,\"battery\":3.85,\"RSSI\":-76.2}");
   myBleSender->sendGravitymonData(json);
   
   int counter = 0;
@@ -51,6 +65,11 @@ void loop() {
     }
     counter ++;   
   }
+#endif
+
+#if defined(CLIENT)
+  bleScanner.scan();
+  bleScanner.waitForScan();
 #endif
 
   loopCounter++;
